8f67c9cee01dc05a6b600b5c209269dad1d12baf
thedon122 <donovan122@yahoo.com>
2021-03-19 00:12:09 -0400

    controller error func for login

--- database/controllers/authController.go
+++ database/controllers/authController.go
@@ -30,3 +30,21 @@ func Register(c *fiber.Ctx) error {
 	database.DB.Create(&user)
 
 	return c.JSON(user)
+}
+
+func Login(c *fiber.Ctx) error {
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	var user models.User
+
+	database.DB.Where("email = ?", data["email"]).First(&user)
+
+	if user.Id == 0 {
+		c.Status(fiber.StatusNotFound)
+		return c.JSON(fiber.Map{
+			"message": "user not found",
+		})
