5ed0d019b14837e90efbcea751a56c24e8ed678d
thedon122 <donovan122@yahoo.com>
2021-03-18 23:58:46 -0400

    controller func for error check

--- database/controllers/authController.go
+++ database/controllers/authController.go
@@ -9,3 +9,14 @@ import (
 	"strconv"
 	"time"
 )
+
+const SecretKey = "secret"
+
+func Register(c *fiber.Ctx) error {
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 14)
