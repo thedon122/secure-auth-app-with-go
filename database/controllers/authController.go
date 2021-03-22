f6b7404063be27ca957f62f22aac2a263bd65d7d
thedon122 <donovan122@yahoo.com>
2021-03-21 23:14:43 -0400

    controller user func

--- database/controllers/authController.go
+++ database/controllers/authController.go
@@ -84,3 +84,18 @@ func Login(c *fiber.Ctx) error {
 		"message": "success",
 	})
 }
+
+
+func User(c *fiber.Ctx) error {
+	cookie := c.Cookies("jwt")
+
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(SecretKey), nil
+	})
+
+	if err != nil {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "unauthenticated",
+		})
+	}
